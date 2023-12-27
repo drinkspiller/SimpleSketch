f4db8189608adef69f7cb81a29a784d3ccb8100b
Skye Giordano <skyebot@google.com>
2023-12-27 12:08:37 -0700

    Initial commit

---
+++ src/app/simple-sketch/ng-simple-sketch.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ng-simple-sketch',
+  standalone: true,
+  imports: [],
+  templateUrl: './ng-simple-sketch.component.html',
+  styleUrl: './ng-simple-sketch.component.scss',
+})
+export class NgSimpleSketchComponent {}
