2b97e994cd2580525f2a7743942bc7afaa6b4beb
Skye Giordano <skyebot@google.com>
2023-12-27 14:26:32 -0700

    Simple sketch component added to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import {CommonModule} from '@angular/common';
 import {Component} from '@angular/core';
+import {NgSimpleSketchComponent} from './simple-sketch/ng-simple-sketch.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NgSimpleSketchComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-}
+export class AppComponent {}
