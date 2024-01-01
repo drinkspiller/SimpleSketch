52288e13af9990500d99b013d154decae9091c7a
Skye Giordano <skyebot@google.com>
2024-01-01 12:41:20 -0700

    Renames simple-sketch.component to include 'canvas' in name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import {CommonModule} from '@angular/common';
 import {Component} from '@angular/core';
-import {SimpleSketchComponent} from './simple-sketch/simple-sketch.component';
+import {SimpleSketchCanvasComponent} from './simple-sketch/simple-sketch-canvas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, SimpleSketchComponent],
+  imports: [CommonModule, SimpleSketchCanvasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
