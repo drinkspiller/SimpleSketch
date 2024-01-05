53580b2e66a11496a8381c185730408c5402bf8a
Skye Giordano <skyebot@google.com>
2024-01-05 12:39:39 -0700

    Adds css debug handling based on query string

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import {CommonModule} from '@angular/common';
-import {Component} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
 import {SimpleSketchCanvasComponent} from '../simple-sketch/canvas/canvas.component';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {WINDOW} from '../simple-sketch/injection-tokens';
+import {PromptInputComponent} from '../prompt-input/prompt-input.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, SimpleSketchCanvasComponent],
+  imports: [CommonModule, PromptInputComponent, SimpleSketchCanvasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private readonly window: Window = inject(WINDOW);
+  private readonly isDebug = new BehaviorSubject<boolean>(false);
+  protected readonly isDebug$: Observable<boolean> = this.isDebug;
+
+  ngOnInit() {
+    // eslint-disable-next-line node/no-unsupported-features/node-builtins
+    const urlParams = new URLSearchParams(this.window.location.search);
+    this.isDebug.next(urlParams.get('css-debug') ? true : false);
+  }
+}
